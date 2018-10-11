f579018932fbd310c4c6bac4978feb07b677349e
sch <sch@myfantasy.ru>
2018-10-11 13:14:10 +0300

    copy file

--- main.go
+++ main.go
@@ -1,46 +1,98 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/myfantasy/mfe"
+	"github.com/myfantasy/mfetl"
+
+	"github.com/myfantasy/mfh"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	var version = flag.Bool("version", false, "return current version")
-	var verbose = flag.Bool("v", false, "verbose (default 0)\n1 - verbose\nother not verbose")
-	var mode = flag.String("m", "", `mode (default o)
-o - one action
-qcp - LoadAndInsertFromQuery params (dtf, dfcs, dtt, dtcs, q, tt)`)
-
-	var dtf = flag.String("dtf", "", "dbTypeFrom")
-	var dfcs = flag.String("dfcs", "", "dbFromCS")
-	var dtt = flag.String("dtt", "", "dbTypeTo")
-	var dtcs = flag.String("dtcs", "", "dbToCS")
-	var q = flag.String("q", "", "queryFrom")
-	var tt = flag.String("tt", "", "tableTo")
-
-	flag.Parse()
-
-	if *version {
+	s := loadSettings()
+	if *s.version {
 		fmt.Println("version 0.0.1")
 		return
 	}
 
-	if *verbose {
-		fmt.Println("mode: " + *mode)
-		fmt.Println("dtf: " + *dtf)
-		fmt.Println("dfcs: " + *dfcs)
-		fmt.Println("dtt: " + *dtt)
-		fmt.Println("dtcs: " + *dtcs)
-		fmt.Println("q: " + *q)
-		fmt.Println("tt: " + *tt)
+	if *s.mode == "http" {
+		httpServiceRun(s)
+	} else if *s.mode == "file" {
+		fileSettings(s)
+	} else {
+		fmt.Println("mode not found")
+		return
 	}
 
-	if *mode == "qcp" {
-		err := LoadAndInsertFromQuery(*dtf, *dfcs, *dtt, *dtcs, *q, *tt, *verbose)
-		if err != nil {
-			fmt.Println(err)
+}
+
+func httpServiceRun(s settings) {
+	mfe.LogActionF("", "etl.httpServiceRun", "start")
+	httpPort := *s.httpPort
+
+	r := mfh.Route{}
+
+	api := http.Server{
+		Addr:           fmt.Sprintf(":%d", httpPort),
+		Handler:        &r,
+		ReadTimeout:    5e9,
+		WriteTimeout:   5e9,
+		MaxHeaderBytes: 16 << 20, // 16Mb
+	}
+
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Infof("Listen and serve %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Can`t start server; %v", err)
+
+	case <-osSignals:
+		log.Infof("Start shutdown...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5e9)
+		defer cancel()
+
+		if err := api.Shutdown(ctx); err != nil {
+			log.Infof("Graceful shutdown did not complete in 5s : %v", err)
+			if err := api.Close(); err != nil {
+				log.Fatalf("Could not stop http server: %v", err)
+			}
 		}
+	}
+}
+
+func fileSettings(s settings) {
+
+	data, err := ioutil.ReadFile(*s.file)
+
+	if err != nil {
+		mfe.LogExtErrorF(err.Error(), "etl.fileSettings", "load file")
+	}
+
+	v, err := mfe.VariantNewFromJSON(string(data))
+
+	if err != nil {
+		mfe.LogExtErrorF(err.Error(), "etl.fileSettings", "parse file")
+	}
+
+	method := v.GE("method").Str()
+
+	if method == "copy" {
+		mfetl.CopyTable(v)
 	}
 
 }
---
+++ settings.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/myfantasy/mfe"
+	log "github.com/sirupsen/logrus"
+)
+
+type settings struct {
+	version  *bool
+	verbose  *bool
+	logLevel *int
+	httpPort *int
+	mode     *string
+	file     *string
+}
+
+func loadSettings() (s settings) {
+	s.version = flag.Bool("version", false, "return current version")
+	s.verbose = flag.Bool("v", false, "verbose (default 0)\n1 - verbose\nother not verbose")
+	s.logLevel = flag.Int("loglevel", int(log.InfoLevel), "loglevel (default 4)\n0 - PanicLevel\n1 - FatalLevel\n2 - ErrorLevel\n3 - WarnLevel\n4 - InfoLevel\n5 - DebugLevel")
+	s.mode = flag.String("m", "http", `mode (default http)
+	http - http service
+	file - load file and `)
+	s.httpPort = flag.Int("port", 8080, "Http Port (default 8080)")
+	s.file = flag.String("fn", "settings.json", `file name for file mode (default settings.json)`)
+
+	flag.Parse()
+
+	log.SetLevel(log.Level(*s.logLevel))
+
+	if *s.verbose {
+		mfe.ActionLevelSet(log.InfoLevel)
+		fmt.Println("Started")
+		fmt.Println(time.Now())
+		fmt.Println("Mode:", *s.mode)
+		fmt.Println("loglevel:", *s.logLevel)
+		fmt.Println("port:", *s.httpPort)
+		fmt.Println("fn:", *s.file)
+		return
+	}
+	return s
+}
